2a4b1f754a307bf3d0ac7f750a99ab951511f108
Arthur Silva <arthur.gbs.dev@gmail.com>
2025-12-28 23:55:04 -0300

    correções pontuais em alguns trechos de testes

--- src/test/java/jdbc/FabricaConexoesTest.java
+++ src/test/java/jdbc/FabricaConexoesTest.java
@@ -15,15 +15,9 @@ class FabricaConexoesTest {
     @Test
     void getConexao() {
         try (Connection conn = FabricaConexoes.getConexao()) {
-
-            // Verifica se o objeto foi criado (não é null)
             Assertions.assertNotNull(conn, "A conexão não deveria ser nula");
-
-            // Verifica se a conexão está realmente ativa no MySQL
             Assertions.assertFalse(conn.isClosed(), "A conexão deveria estar aberta");
-
         } catch (SQLException e) {
-            // Se cair aqui, o teste falha automaticamente
             Assertions.fail("Falha ao conectar ao banco: " + e.getMessage());
         }
     }
